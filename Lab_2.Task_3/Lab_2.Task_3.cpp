2012b7fdcf5a9e49788badc14bc3cb950e2c1287
Lipoffv <lipov.y@mail.ru>
2020-09-13 14:27:11 +0300

    Added Task 3 from Lab 2

--- Lab_2.Task_3/Lab_2.Task_3.cpp
+++ Lab_2.Task_3/Lab_2.Task_3.cpp
@@ -1,7 +1,39 @@
 ﻿#include <iostream>
 
-int main()
+using namespace std;
+
+void max_min(int a, int b, int c, int& min, int& max)
 {
-    std::cout << "Hello World!\n";
+    if (b < min)
+    {
+        min = b;
+        if (c < min)
+            min = c;
+    }
+    else if (c < min)
+        min = c;
+
+    if (b > max)
+    {
+        max = b;
+        if (c > max)
+            max = c;
+    }
+    else if (c > max)
+        max = c;
 }
 
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b, c;
+    int min;
+    int max;
+    cout << "\nВведите первое число: "; cin >> a;
+    min = a; max = a;
+    cout << "\nВведите второе число: "; cin >> b;
+    cout << "\nВведите третье число: "; cin >> c;
+    max_min(a, b, c, min, max);
+    cout << "min: " << min << "\nmax: " << max << endl;
+    return 0;
+}
