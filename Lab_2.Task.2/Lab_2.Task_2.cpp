e76f9c39a6c2f5c67c290d297b9d68d1d97940e5
Lipoffv <lipov.y@mail.ru>
2020-09-13 17:00:45 +0300

    Upd

---
+++ Lab_2.Task.2/Lab_2.Task_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int degree(int N, int K)
+{
+    int sum{0};
+    while (N > 0)
+    {
+        sum += pow(N % 10, K);
+        N /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    long int N, K;
+    cout << "Enter N: "; cin >> N;
+    cout << "\nEnter K: "; cin >> K;
+    if (N == degree(N, K))
+        cout << " Число N РАВНО сумме степеней K своих цифр" << endl;
+    else
+        cout << " Число N НЕ РАВНО сумме степеней K своих цифр" << endl;
+    return 0;
+}
+
